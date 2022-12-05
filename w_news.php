7ed19b1fb94cde785cba56b7fe48818e53a50dcc
liza vetrova <lizochka8let@yandex.ru>
2022-12-06 02:22:54 +0300

    first2

--- w_news.php
+++ w_news.php
@@ -1,6 +1,24 @@
+//// name: webest news
+/// link: t.me/webestnewsbot
+//// API: 5851168804:AAEUUhcEuuzmx_d-iGCI8KxwhxHKDLvWBP8
 <?
-include('clas.php');
+include('wb_n_clas.php');
+$bot_api_key = '5851168804:AAEUUhcEuuzmx_d-iGCI8KxwhxHKDLvWBP8';
+$bot_username = 'webest news';
 
+
+попробуйте {
+ // Создать объект API Telegram
+     $telegram = новый  Longman \ TelegramBot \ Telegram ($bot_api_key, $bot_username);
+
+ // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+ if ($result->isOk()) { 
+echo $result->getDescription();
+ }
+} catch (Longman \ TelegramBot \ Exception \ TelegramException  $ e) {
+ // регистрировать ошибки telegram
+    // echo $e->GetMessage();
 $aut=new db ('mysql:host=localhost;dbname=tv','root','root');
 
 print('<!DOCTYPE html><html> 
